463c1ff141df885fb8f95d6d8e22467b9ff69bd2
clevok <1029794653@qq.com>
2019-11-13 12:06:48 +0800

    0.1.1

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import CallbackQueue from './callback-queue';
 import EventLoop from './event-loop';
 
-export default class Index {
-
-    /** 任务池 */
-    static CallbackQueue = CallbackQueue;
-    
-    /** 事件处理中心 */
-    static EventLoop = EventLoop;
-}
 
 export const Queue = CallbackQueue;
 export const Loop = EventLoop;
+
+/**
+ * 默认对外导出一个没有上限的任务池,每次执行一个,超时事件15秒
+ */
+export default new Queue().addEventListener(new Loop());
